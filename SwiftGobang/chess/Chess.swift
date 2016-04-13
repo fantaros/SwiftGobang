bf3c9d027eec9a566d658c463d9dea54671371ee
Yan Luhan <yan.luhan@sfit.shfe.com.cn>
2016-04-13 07:03:06 -0700

    change name to chess

---
+++ SwiftGobang/chess/Chess.swift
@@ -0,0 +1,13 @@
+//
+//  Chess.swift
+//  SwiftGobang
+//
+//  Created by fantaros on 16/4/12.
+//  Copyright © 2016年 fantaros. All rights reserved.
+//
+
+import Foundation
+
+protocol Chess {
+    var chessName : String {get set}
+}
---
+++ SwiftGobang/chess/ChessView.swift
@@ -0,0 +1,13 @@
+//
+//  ChessView.swift
+//  SwiftGobang
+//
+//  Created by fantaros on 16/4/13.
+//  Copyright © 2016年 fantaros. All rights reserved.
+//
+
+import UIKit
+
+class ChessView : UIView {
+
+}
